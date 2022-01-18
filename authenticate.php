3c28369e4bb830aa4f6e0095b6864d61f99df0a3
mehrzaden <mehrzaden@gmail.com>
2022-01-18 11:43:29 +0330

    security information disclosure repaired

--- authenticate.php
+++ authenticate.php
@@ -1,15 +1,17 @@
 <?php
 session_start();
 // Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_HOST = 'localhost';#hostaddress
+$DATABASE_USER = 'root';#database managemaent admin username 
+$DATABASE_PASS = '';# database managemaent admin password 
+$DATABASE_NAME = 'phplogin';#database name 
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to DataBase System Management: ' . mysqli_connect_error());
+exit('Failed to connect to DataBase System Management ! ' /*. mysqli_connect_error(/*forsecurity reasons the error message should not be present
+     any informational data souch as os type version or database managment <type>
+version ,so the . mysqli_connect_error() must be commented )*/);
 }
 
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
@@ -29,7 +31,7 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 
 
 
-if ($stmt->num_rows > 0) {
+if ($stmt->num_rows > 0) { 
     $stmt->bind_result($id, $password);
     $stmt->fetch();
     // Account exists, now we verify the password.
